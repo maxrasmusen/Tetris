18060409359e297f7ad809449edb3fd0c3626b88
Max Rasmusen <mrasmusen@spartaglobal.co>
2017-05-22 21:00:16 +0100

    added a bunch of comments to clarify some of the code

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ function init() {
 }
 
 function tick(mainBoard, HTMLBoard) {
+	//every tick, the block falls by one
 	var drawing = true;
 	clearBoard(mainBoard);	
 	if (checkBlock(currentBlock, mainBoard)) {
@@ -41,6 +42,7 @@ function tick(mainBoard, HTMLBoard) {
 }
 
 function generateBlock () {
+	//create a new block when old one hits the ground
 	return {
 		x : 0,
 		y : 13,
@@ -49,6 +51,8 @@ function generateBlock () {
 }
 
 function placeBlock(block, board) {
+	// places the block on the ground
+	// this is needed so this block stays where it is permanently, won't be refreshed each tick
 	var array = block.array;
 	var x = block.x;
 	var y = block.y;
@@ -62,6 +66,7 @@ function placeBlock(block, board) {
 }
 
 function checkBlock(currentBlock, board) {
+	// checks if there is space below the block for it to fall once
 	currentBlock.y += 1;
 	var array = currentBlock.array;
 	if (currentBlock.y + array.length > board.length) {
@@ -80,10 +85,12 @@ function checkBlock(currentBlock, board) {
 }
 
 function drawBlock(block, board) {
+	// draws current falling block onto board
 	overlayArray(block["array"], board, block.x, block.y);
 }
 
 function generateBoard() {
+	//creates inital board. this is the board the logic is done on. a 2d array
 	var mainBoard = [];
 		for (var i=0; i < 20; i++) {
 			mainBoard[i] = [];
@@ -95,6 +102,7 @@ function generateBoard() {
 }
 
 function setUpHTMLBoard() {
+	//creates inital HTML board. This is a bunch of html elements that are updated using the mainbBoard
 	var board = $('#main');
 	for (var i = 0; i < 20; i++) {
 		var row = $('<ul>').attr('id', 'row-' + i);
@@ -107,6 +115,8 @@ function setUpHTMLBoard() {
 }
 
 function clearBoard(board) {
+	// clears the board of all temporary pieces each tick
+
 	// board.forEach(function (row) {
 	// 	row.forEach(function(square) {
 	// 		console.log(square);
@@ -125,6 +135,8 @@ function clearBoard(board) {
 }
 
 function overlayArray(array, board, x, y) {
+	// translates the block array onto the board. 
+
 	for (var i = 0; i < array.length; i++) {
 		for (var j = 0; j < array[i].length; j++) {
 			// console.log(x + ', ' + y + ', ' + i + ', ' + j)
@@ -137,6 +149,8 @@ function overlayArray(array, board, x, y) {
 }
 
 function showBoard(board, HTMLBoard) {
+	// takes the mainBoard and updates the html elements so we can see what's happening.
+
 	HTMLBoard.children().toArray().forEach(function (row, rowIndex) {
 		// console.log(row);
 		$(row).children().toArray().forEach (function (square, squareIndex) {
